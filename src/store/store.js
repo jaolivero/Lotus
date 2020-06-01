aa5de611ae4c0ae48c9be1578b850a95cda65a2f
joel olivero <joelolivero@outlook.com>
2020-05-31 23:55:53 -0400

    Added redux to application and also created store

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk;";
+
+const INITIAL_STATE = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducers,
+  INITIAL_STATE,
+  compose(applyMiddleware(...middleware))
+);
