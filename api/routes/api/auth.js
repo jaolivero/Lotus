5526bd5508be03f48ffe3bda56d7aa60b058a5f5
joel olivero <joelolivero@outlook.com>
2020-06-04 23:58:52 -0400

    getting auth user

--- api/routes/api/auth.js
+++ api/routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require('../../models/Users')
 
-//@Route Get api/auth
+//@Route Get api/auth/user
 // desc Test route
 //@access Public
-router.get("/", (req, res) => res.send("auth route"));
+router.get("/", auth, async (req, res) => try {
+        try {
+    const user = await User.findById(req.user.id.select('-password');
+    res.json(user);
+} catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
